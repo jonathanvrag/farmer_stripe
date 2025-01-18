ec7f7d590f0ac3028a25d626885ba7bf40f64fdc
jonathanvra <jonathan.vra@gmail.com>
2025-01-18 14:06:09 -0500

    feat: I added an error message to validate access to the Stripe API

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const $courses = document.getElementById('courses');
 const $template = document.getElementById('coursesCard-template').content;
 const $fragment = document.createDocumentFragment();
 const options = { headers: { Authorization: `Bearer ${KEYS.secret}` } };
-const formatPrice = num => `${num.slice(0, -2)}.${num.slice(-2)}`
+const formatPrice = num => `${num.slice(0, -2)}.${num.slice(-2)}`;
 
 let products, prices;
 
@@ -29,11 +29,18 @@ Promise.all([
       $template.querySelector('img').alt = productData[0].name;
       $template.querySelector('figcaption').innerHTML = `${
         productData[0].name
-      } ${formatPrice(element.unit_amount_decimal)} ${element.currency.toUpperCase()}`;
+      } ${formatPrice(
+        element.unit_amount_decimal
+      )} ${element.currency.toUpperCase()}`;
 
       let $clone = document.importNode($template, true);
       $fragment.appendChild($clone);
     });
 
     $courses.appendChild($fragment);
+  })
+  .catch(error => {
+    let message = error.statusText || 'An error occurred in the request';
+
+    $courses.innerHTML = `Error ${error.status}: ${message}`;
   });
